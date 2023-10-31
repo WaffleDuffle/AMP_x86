10de5562766f801cdb0330908f5c7d36c8184eed
Constantin Denis <denis.io.constantin@gmail.com>
2023-10-31 23:11:19 +0200

    Update lab1_prog1.asm

--- Lab1/P1/lab1_prog1.asm
+++ Lab1/P1/lab1_prog1.asm
@@ -1,16 +1,19 @@
 org 100h
 
-mov AX, 0h
-mov DX, 0h
+init: 
+      mov AX, 0h
+      mov DX, 0h
 
-mov AX, 1234h
-add AX, 8017h
-adc DX, 0h
-add AX, 9423h
-adc DX, 0h
+sum:
+    mov AX, 1234h
+    add AX, 8017h
+    adc DX, 0h
+    add AX, 9423h
+    adc DX, 0h
 
-mov BX, 3h
-div BX
-mov CX, AX
+mean:
+    mov BX, 3h
+    div BX
+    mov CX, AX
 
 int 20h
